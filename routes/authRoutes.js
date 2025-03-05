972321ad219d5c74e2a74ba861b2176e772827f9
Archit-KineticScape <archit@kineticscapestudios.com>
2025-03-05 16:23:30 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { kylasCallback } = require("../controllers/authController");
+const { kylasCallback, update_API } = require("../controllers/authController");
 const router = express.Router();
 if (!kylasCallback) {
   throw new Error("kylasCallback function is not defined in authController");
 }
 router.post("/kylas/callback", kylasCallback);
-router.post("/update-api");
+router.post("/update-api", update_API);
 module.exports = router;
